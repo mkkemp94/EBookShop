4b4465ac6590a1a2ba4c777135b8b5cc0e7948fd
MKemp <mkemp@accu-time.com>
2021-01-09 16:21:30 -0500

    Add ability to add, edit, and delete books

--- app/src/main/java/com/mkemp/ebookshop/MainActivity.java
+++ app/src/main/java/com/mkemp/ebookshop/MainActivity.java
@@ -1,12 +1,12 @@
 package com.mkemp.ebookshop;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 import com.mkemp.ebookshop.databinding.ActivityMainBinding;
 import com.mkemp.ebookshop.model.Book;
@@ -16,11 +16,14 @@ import com.mkemp.ebookshop.viewmodel.MainActivityViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -37,6 +40,11 @@ public class MainActivity extends AppCompatActivity
     
     private ArrayList<Category> categoryList;
     private ArrayList<Book> bookList;
+    
+    public static final int ADD_BOOK_REQUEST_CODE = 1;
+    public static final int EDIT_BOOK_REQUEST_CODE = 2;
+    
+    private int selectedBookId;
     
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -91,13 +99,42 @@ public class MainActivity extends AppCompatActivity
         booksRecyclerView.setAdapter(booksAdapter);
         
         booksAdapter.setBooks(bookList);
+        booksAdapter.setListener(new BooksAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Book book)
+            {
+                selectedBookId = book.getBookId();
+                
+                Intent intent = new Intent(MainActivity.this, AddAndEditActivity.class);
+                intent.putExtra(AddAndEditActivity.BOOK_ID, selectedBookId);
+                intent.putExtra(AddAndEditActivity.BOOK_NAME, book.getBookName());
+                intent.putExtra(AddAndEditActivity.UNIT_PRICE, book.getUnitPrice());
+                startActivityForResult(intent, EDIT_BOOK_REQUEST_CODE);
+            }
+        });
+        
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target)
+            {
+                return false;
+            }
+    
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction)
+            {
+                Book bookToDelete = bookList.get(viewHolder.getAdapterPosition());
+                mainActivityViewModel.deleteBook(bookToDelete);
+            }
+        }).attachToRecyclerView(booksRecyclerView);
     }
     
     public class MainActivityClickHandlers
     {
         public void onFABClicked(View view)
         {
-            Toast.makeText(getApplicationContext(), "FAB clicked", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this, AddAndEditActivity.class);
+            startActivityForResult(intent, ADD_BOOK_REQUEST_CODE);
         }
         
         public void onSelectItem(AdapterView<?> parent, View view, int pos, long id)
@@ -130,5 +167,30 @@ public class MainActivity extends AppCompatActivity
         }
         
         return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+    
+        int selectedCategoryId = selectedCategory.getId();
+        if ( requestCode == ADD_BOOK_REQUEST_CODE && resultCode == RESULT_OK )
+        {
+            Book book = new Book();
+            book.setCategoryId(selectedCategoryId);
+            book.setBookName(data.getStringExtra(AddAndEditActivity.BOOK_NAME));
+            book.setUnitPrice(data.getStringExtra(AddAndEditActivity.UNIT_PRICE));
+            mainActivityViewModel.addNewBook(book);
+        }
+        else if ( requestCode == EDIT_BOOK_REQUEST_CODE && resultCode == RESULT_OK )
+        {
+            Book book = new Book();
+            book.setCategoryId(selectedCategoryId);
+            book.setBookName(data.getStringExtra(AddAndEditActivity.BOOK_NAME));
+            book.setUnitPrice(data.getStringExtra(AddAndEditActivity.UNIT_PRICE));
+            book.setBookId(selectedBookId);
+            mainActivityViewModel.updateBook(book);
+        }
     }
 }
